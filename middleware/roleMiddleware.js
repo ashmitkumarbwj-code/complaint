0ea3d48d9bd94a49058a7552bbc9e4d28aef5ceb
Ashmit Kumar <ashmitkumarbwj@gmail.com>
2026-03-18 23:04:32 +0530

    Upload: middleware/roleMiddleware.js

---
+++ middleware/roleMiddleware.js
@@ -0,0 +1,11 @@
+module.exports = (roles) => {
+    return (req, res, next) => {
+        if (!req.user || !roles.includes(req.user.role)) {
+            return res.status(403).json({ 
+                success: false, 
+                message: 'Access Denied: You do not have permission to perform this action' 
+            });
+        }
+        next();
+    };
+};
